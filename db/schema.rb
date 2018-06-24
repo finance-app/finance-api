27eb6038a69b8d7e79504904110eadb29b1957d8
Mateusz Gozdek <mgozdekof@gmail.com>
2018-06-24 22:18:56 +0200

    Add missing database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604223834) do
+ActiveRecord::Schema.define(version: 20180624114154) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name", null: false
